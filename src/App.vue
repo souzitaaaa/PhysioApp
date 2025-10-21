c507c604d9a83ccb78aaf2ad3c62860f88cdd723
Diogo <diogo.nogueirasousa123@gmail.com>
2025-10-22 00:56:54 +0100

    Testing git branches

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="card flex justify-center">
     <Button label="Submit" />
+    <Button label="Ferreira" />
+    <ToggleSwitch v-model="checked" />
     <ToggleSwitch v-model="checked" />
   </div>
 </template>
