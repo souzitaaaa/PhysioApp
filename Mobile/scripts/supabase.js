be1d6adfec3dcc3fd8a44c59dbf5c2be28ae1e0a
f3rr3ira <rodrigo.pintoff04@gmail.com>
2025-12-04 01:26:49 +0000

    mobile-email-calendar

---
+++ Mobile/scripts/supabase.js
@@ -0,0 +1,7 @@
+import 'cross-fetch/polyfill';
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = 'https://bxzxbtkdgejmvdzeciqe.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4enhidGtkZ2VqbXZkemVjaXFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwMzIyMTIsImV4cCI6MjA3ODYwODIxMn0.0yr4PU3L7692h9prk4deBGOV5I1EiXG2qBuHPhFrynQ';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
