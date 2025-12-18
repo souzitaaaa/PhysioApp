d8131c42f1f7246a34931c826ac891fc620094cb
souzitaaaa <diogo.nogueirasousa123@gmail.com>
2025-12-18 19:33:30 +0000

    AI Logic and implementation done

--- backend/controllers/emailsController.js
+++ backend/controllers/emailsController.js
@@ -21,3 +21,63 @@ export async function getEmailByID(req, res) {
 	if (error) return res.status(500).json({ error });
 	return res.json(data);
 }
+
+// GET | Email with ID already exists
+export async function getEmailExists(emailID) {
+	const { data, error } = await supabase
+		.from("t_email")
+		.select("realEmailID")
+		.eq("realEmailID", emailID)
+		.limit(1)
+		.maybeSingle();
+
+	if (error) throw error;
+
+	// If null = false | If {} = true
+	return !!data;
+}
+
+// GET | Existing Email have injuryRecord created
+export async function getExistingEmailHasRecord(emailID) {
+	const { data, error } = await supabase
+		.from("t_email")
+		.select("realEmailID")
+		.eq("realEmailID", emailID)
+		.not("injuryRecordID", "is", null)
+		.limit(1)
+		.maybeSingle();
+
+	if (error) throw error;
+
+	// If null = false | If {} = true
+	return !!data;
+}
+
+// POST | Creates email from gmail API
+export async function createEmail(email) {
+
+	const { data, error } = await supabase
+		.from("t_email")
+		.insert({
+			realEmailID: email.id,
+			from: email.from,
+			subject: email.subject,
+			body: email.body,
+			date: new Date(email.date),
+		});
+
+	if (error) throw error;
+
+	return { created: true };
+}
+
+// UPDATE | Email with RecordID just created
+export async function updateEmailWithRecord(realEmailID, injuryRecordID) {
+	const { data, error } = await supabase
+		.from('t_email')
+		.update({ injuryRecordID })
+		.eq('realEmailID', realEmailID);
+
+	if (error) throw error;
+	return { created: true };
+}
